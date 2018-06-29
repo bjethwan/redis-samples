ba392b3a2c9de14aad82f70baf59fe60e2536050
Bipin Jethwani <KH1783@kitspl.com>
2018-06-29 12:35:50 +0700

    while at tmb bank bangkok

--- sample2/src/main/java/sample2/Main.java
+++ sample2/src/main/java/sample2/Main.java
@@ -9,26 +9,24 @@ import redis.clients.jedis.JedisPoolConfig;
 public class Main {
 	public static void main(String[] args) {
 
-		JedisPool pool = new JedisPool(
-				new JedisPoolConfig(), 
-				"ec2-34-239-185-146.compute-1.amazonaws.com");
-		
+		JedisPool pool = new JedisPool(new JedisPoolConfig(), "ec2-13-250-2-176.ap-southeast-1.compute.amazonaws.com", 6379);
+
 		try (Jedis jedis = pool.getResource()) {
-			 
-			  jedis.set("foo", "bar");
-			  String foobar = jedis.get("foo");
-			  
-			  System.out.println(foobar);
-			  
-			  jedis.zadd("sose", 0, "car"); 
-			  jedis.zadd("sose", 0, "bike"); 
-			  
-			  Set<String> sose = jedis.zrange("sose", 0, -1);
-			  
-			  System.out.println(sose);
-			}
-			
-			/// ... when closing your application:
-			pool.close();
+
+			jedis.set("foo", "bar");
+			String foobar = jedis.get("foo");
+
+			System.out.println(foobar);
+
+			jedis.zadd("items", 0, "car"); 
+			jedis.zadd("items", 0, "bike"); 
+
+			Set<String> items = jedis.zrange("items", 0, -1);
+
+			System.out.println(items);
+		}
+
+		/// ... when closing your application:
+		pool.close();
 	}
 }
